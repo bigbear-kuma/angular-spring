bddd55749abe93a548f2337d59e460e615dbe061
大熊拓実 <takumi.okuma.5s@stu.hosei.ac.jp>
2025-07-09 00:39:16 +0900

    再度実行時にでたバグを修正

--- connect/src/main/java/com/example/demo/mapper/HeroMapper.java
+++ connect/src/main/java/com/example/demo/mapper/HeroMapper.java
@@ -4,11 +4,10 @@ import java.util.List;
 //import com.example.demo.entity.Hero;
 import com.example.demo.entity.Hero;   
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+
 
 
 @Mapper  // もしくは @MapperScan を使う
 public interface HeroMapper {
-  @Select("SELECT id, name FROM heroes")
   List<Hero> findAll();
 }
